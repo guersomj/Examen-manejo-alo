6bfeae860518a6e57d97a46fa108e6da6ec6e275
guersomj <2503230625@cesunbc.edu.mx>
2026-04-20 10:52:29 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Examen de Manejo Alo", page_icon="🚗")
+
+st.title("🚗 Examen de Manejo Alo")
+st.write("App de práctica para licencia de conducir en Baja California.")
+st.write("Próximamente: exámenes aleatorios de 10 preguntas.")
